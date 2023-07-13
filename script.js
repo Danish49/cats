a56d04186034f67f6c8661a1c5398d5e035b44da
Danish Mushtaq <danishmushtaq7870@gmail.com>
2023-07-13 11:48:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const requestbtn = document.querySelector("#btn");
+const adviceId = document.querySelector(".advNumber");
+const adviceText = document.querySelector(".adviceText");
+
+const sendRequest = async () => {
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    const requestId = response.data.fact.length;
+    const requestText = response.data.fact;
+    adviceId.innerText = `Advice #${requestId}`;
+    adviceText.innerText = `"${requestText}"`;
+  } catch (e) {
+    adviceText.innerText = `"${e}"`;
+  }
+};
+
+requestbtn.addEventListener("click", () => {
+  sendRequest();
+});
